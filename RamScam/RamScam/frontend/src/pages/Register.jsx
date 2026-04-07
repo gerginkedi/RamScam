feabb8ef9a2b740628b411a34b96fcc971371c22
Erdem Eray Öztürk <143835599+OerdM@users.noreply.github.com>
2026-04-07 18:31:48 +0300

    added login page and routing, also added some dependencies for the login page.

--- RamScam/RamScam/frontend/src/pages/Register.jsx
+++ RamScam/RamScam/frontend/src/pages/Register.jsx
@@ -4,7 +4,28 @@ import '../styles/index.css'
 
 function Register() {
   return (
-    <div></div>
+    <div className="login-container">
+      <div className="flex-container">
+        <form className='ust-row'>
+            <label>E-posta Adresi</label>
+            <div>
+                <input type='text' placeholder='ornek@gmail.com' autoCapitalize='off' autoCorrect='off'/>
+            </div>
+            <label>Şifre</label>
+            <div>   
+                <input type='text' placeholder='••••••••' autoCapitalize='off' autoCorrect='off'/>
+            </div>
+            <label>Şifre Tekrar</label>
+            <div>   
+                <input type='text' placeholder='' autoCapitalize='off' autoCorrect='off'/>
+            </div>            
+        </form>
+        <div className="alt-row">
+            <button id='register'>Kayıt Ol</button>
+            <h5>Zaten bir hesabınız var mı? <a href="/login">Giriş yapın</a></h5>
+        </div>       
+      </div>
+    </div>
   )
 }
 
