2caa9d86fa9e15a01fe1397bad8168a18db0a7b2
Erdem Eray Öztürk <143835599+OerdM@users.noreply.github.com>
2026-04-10 18:46:44 +0300

    added a new swiper section to the home page, added some styling to the layout and home css files, and updated the package.json and package-lock.json to include the swiper dependency.

--- RamScam/RamScam/frontend/src/pages/Home.jsx
+++ RamScam/RamScam/frontend/src/pages/Home.jsx
@@ -1,11 +1,52 @@
 import '../styles/layout.css';
+import '../styles/Home.css';
 import Layout from '../components/layout';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 
 function Home() {
     return (
         <Layout>
-            <div className="home-container">
+            <div className="top-games-swiper">
+                <h2>Top Games</h2>
+                <Swiper 
+                    modules={[Navigation]}
+                    navigation
+                    loop
+                    spaceBetween={20}
+                    slidesPerView={3}
+                >   
+                    <SwiperSlide>
+                        <div className="game-card">
+                            <img src="https://via.placeholder.com/300x150" alt="Game A" />
+                            <div className='game-info'>
+                                <h3>Game A</h3>
+                            </div>
+                            <button>Oyna</button>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="game-card">
+                            <img src="https://via.placeholder.com/300x150" alt="Game B" />
+                            <div className='game-info'>
+                                <h3>Game B</h3>
+                            </div>
+                            <button>Oyna</button>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="game-card">
+                            <img src="https://via.placeholder.com/300x150" alt="Game C" />
+                            <div className='game-info'>
+                                <h3>Game C</h3>
+                            </div>
+                            <button>Oyna</button>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
             </div>
         </Layout>
     )
