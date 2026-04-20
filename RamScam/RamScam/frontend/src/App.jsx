1be72da650120dd0124f4844797564596580d61a
SSForC <suleymankacmaz9@gmail.com>
2026-04-20 22:18:42 +0300

    ai fun fact widget eklendi

--- RamScam/RamScam/frontend/src/App.jsx
+++ RamScam/RamScam/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Home from "./pages/Home"
 import CoinFlip from "./pages/CoinFlip"
+import AiChatWidget from "./components/AiChatWidget"
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/games/coinflip" element={<CoinFlip />} />
         </Routes>
+
+        {/* Widget'ı buraya (Routing haricine ama tarayıcı içine) koyuyoruz. 
+            Böylece kullanıcı hangi sayfaya giderse gitsin sağ altta hep duracak! */}
+        <AiChatWidget />
       </BrowserRouter>
     </div>
   )
